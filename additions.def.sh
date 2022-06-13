37f8eb79c481971c01b8da8a26b2d353f5e9e833
Robert Mrosky <rmrosk1@lps.umd.edu>
2022-06-13 10:04:48 -0400

    bug fix

--- additions.def.sh
+++ additions.def.sh
@@ -6,7 +6,7 @@ Bootstrap: localimage
 From: /usr/local/bin/sstpackage-11.1.0-ubuntu-20.04.sif
 
 %post
-	apt install graphviz graphviz-dev texlive-full
+	apt install -y graphviz graphviz-dev texlive-full
 	python3 -m pip install black mypy makefile2dot
 EOF
 
