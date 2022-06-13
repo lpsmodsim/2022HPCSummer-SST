ceb0eb510827863da9a3ad6fa7fd703bb2644db2
Robert Mrosky <rmrosk1@lps.umd.edu>
2022-06-13 09:32:31 -0400

    additional packages

---
+++ additions.def.sh
@@ -0,0 +1,12 @@
+#!/bin/env bash
+
+
+cat > additions.def << EOF
+Bootstrap: localimage
+From: /usr/local/bin/sstpackage-11.1.0-ubuntu-20.04.sif
+
+%post
+	python3 -m pip install black mypy makefile2dot
+EOF
+
+sudo singularity build /usr/local/bin/additions.sif additions.def
